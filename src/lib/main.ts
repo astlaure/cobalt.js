2abfb1aa0c9b38f70bce5e67b156cba939610657
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2023-12-29 14:08:31 -0500

    library setup 🔧

---
+++ src/lib/main.ts
@@ -0,0 +1,1 @@
+export { default as CobaltApp } from './App.tsx';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dtsPlugin from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+      react(),
+      dtsPlugin({ include: ['src/lib'] }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/lib/main.ts',
+      formats: ['es'],
+      fileName: 'main',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
+});
